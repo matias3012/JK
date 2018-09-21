9839e9264543c2fffca32771318975b279a67eb5
JuankStack <Juankstack@gmail.com>
2018-09-21 18:12:08 -0500

    Se crea espacio de comentarios y su autor, se realiza migración

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_224029) do
+ActiveRecord::Schema.define(version: 2018_09_21_230837) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "story"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
